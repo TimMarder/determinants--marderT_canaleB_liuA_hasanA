4cf71557c1b6f1ec0e9224f588347fd6d6b3fa1a
Britni <bcanale@stuy.edu>
2019-04-01 21:34:51 -0400

    Trying to add pie charts

---
+++ app.py
@@ -0,0 +1,17 @@
+#Britni Canale
+#SoftDev1 pd 6
+#K26 -- Getting More REST
+#2018-11-15
+
+
+from flask import Flask, render_template, session, request, url_for, redirect, flash
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
---
+++ templates/home.js
@@ -0,0 +1,19 @@
+import d3;
+
+var svg = d3.select("svg");
+var width = svg.attr("width;
+var height = svg.attr("height");
+var radius = Math.min(width, height) / 2;
+var c = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var  color = d3.scaleOrdinal(['red','yellow','blue','orange','green']);
+
+var pie = d3.pie();
+
+var arc = d3.arc().innerRadius(0).outerRadius(radius);
+
+var arcs = c.selectAll("arc").data(pie(data)).enter().append("c").attr("class", "arc");
+
+arcs.append("path").attr("fill", function(d,i){
+  return color(i);
+}).attr("d", arc);
