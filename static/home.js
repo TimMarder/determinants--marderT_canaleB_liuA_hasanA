d945a54547e8f3ecd01d4216577ce6b79e1146cd
alexliu4 <aliu9@stuy.edu>
2019-04-05 10:33:46 -0400

    sideebar

--- static/home.js
+++ static/home.js
@@ -570,6 +570,18 @@ for(var cat in crashes){
   update()
 }
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+
 //svg = d3.select("body").select("svg")
 //svg.attr("width", "100%")
 //svg.attr("height", "100%")
